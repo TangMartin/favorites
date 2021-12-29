850d363860e1c57ce1beacee09095c493dbbbe42
Martin Tang <martintang8422@gmail.com>
2021-12-28 21:52:00 -0800

    Register Feature Complete

---
+++ frontend/styles/loginStyles.js
@@ -0,0 +1,36 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    inputContainer: {
+        width: '80%',
+    },
+    input: {
+        backgroundColor: 'white',
+        margin: '2%',
+        padding: '2%',
+    },
+    buttonContainer: {
+        marginTop: 20,
+        backgroundColor: 'white',
+        width: '60%',
+    },
+    button: {
+        marginTop: 10,
+        width: '100%',
+        padding: '6%',
+        backgroundColor: '#0782F0',
+    },
+    buttonOutline:{
+
+    },
+    buttonText: {
+        font: 'White',
+        textAlign: 'center',
+        color: 'white',
+    }
+});
