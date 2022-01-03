3adc67e0f4af37904b0fcb63452ba5586f2ad5d0
Martin Tang <martintang8422@gmail.com>
2022-01-02 21:14:32 -0800

    Google Autocomplete & Bottom Tab Integration

--- frontend/screens/SearchScreen.js
+++ frontend/screens/SearchScreen.js
@@ -1,24 +1,62 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React, {useCallback, useMemo, useRef } from 'react'
+import { StyleSheet, Text, View} from 'react-native'
+import { SafeAreaView, Flatlist, KeyboardAwareScrollView } from 'react-native-safe-area-context'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
+import BottomSheet from '@gorhom/bottom-sheet';
+import searchStyles from '../styles/searchStyles'
+
+import {
+    BottomSheetModal,
+    BottomSheetModalProvider,
+  } from '@gorhom/bottom-sheet';
+
 const SearchScreen = () => {
+
+    // ref
+    const bottomSheetRef = useRef(null);
+
+    // variables
+    const snapPoints = useMemo(() => ['25%', '45%'], ['75%']);
+
+    // callbacks
+    const handleSheetChanges = useCallback((index: number) => {
+        console.log('handleSheetChanges', index);
+    }, []);
+
     return (
-        <GooglePlacesAutocomplete
-        placeholder='Search'
-        onPress={(data, details = null) => {
-          // 'details' is provided when fetchDetails = true
-          console.log(data, details);
-        }}
-        query={{
-          key: 'AIzaSyAoqlQ1Mipy61k3DhUoX6B5AikDWmS-3lI',
-          language: 'en',
-        }}
-      />
+            <SafeAreaView style={{flex: 1}}>
+                <GooglePlacesAutocomplete
+                    styles={searchStyles}
+                    placeholder='Restaurants, Landmarks, Parks'
+                    minLength={2}
+                    currentLocation={true}
+                    currentLocationLabel="Current Location"
+                    fetchDetails={true}
+                    enableHighAccuracyLocation={true}
+                    onPress={(data, details = null) => {
+                        // 'details' is provided when fetchDetails = true
+                        console.log(details);
+                    }}
+                    query={{
+                        key: 'AIzaSyAoqlQ1Mipy61k3DhUoX6B5AikDWmS-3lI',
+                        language: 'en',
+                    }}
+                    GooglePlacesDetailsQuery={{
+                        rankby: 'distance'}}
+                />
+                <BottomSheet
+                    ref={bottomSheetRef}
+                    index={1}
+                    snapPoints={snapPoints}
+                    onChange={handleSheetChanges}
+                >
+                    <View>
+                        <Text>Awesome 🎉</Text>
+                    </View>
+                </BottomSheet>
+            </SafeAreaView>
     )
 }
 
 export default SearchScreen
-
-const styles = StyleSheet.create({})
---
+++ frontend/styles/searchStyles.js
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '90%',
+        marginLeft: "5%",
+      },
+      textInputContainer: {
+        flexDirection: 'row',
+      },
+      textInput: {
+        backgroundColor: '#FFFFFF',
+        height: 44,
+        borderRadius: 20,
+        paddingVertical: 5,
+        paddingHorizontal: 20,
+        fontSize: 15,
+        color: '#000000',
+        flex: 1,
+      },
+});
